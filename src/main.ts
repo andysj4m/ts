48034512fb5f76738e60739647e2746d1ce628f4
Andy Johnson <andy.johnson@fourmasters.eu>
2016-11-14 16:53:54 +0000

    Intial Source

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import {Two} from './two';
+import * as _ from 'lodash';
+new Two();
+
+//import * as Rx from 'rxjs';
+//import 'rxjs/add/observable/timer';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+
+Observable.timer(1000)
+    .subscribe(x => console.log(x));
+
+class App {
+    constructor() {
+        console.log(`Working zzz ...`);
+    }
+}
+
+new App();
+
+console.log(_.isArray([1,2]));
